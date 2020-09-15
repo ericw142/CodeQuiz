66827c00ccaca45669d9692b8e598e90da5e5d9f
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-09-15 11:58:55 -0400

    Updated styles

--- Assets/js/highscoreScript.js
+++ Assets/js/highscoreScript.js
@@ -2,11 +2,14 @@ var highscoreDiv = document.getElementById('highscoreDiv');
 var erase = document.getElementById('erase');
 var userInput = document.querySelector('#user');
 var submit = document.querySelector('#submit');
+var scoreSpan = document.getElementById('scoreSpan');
 
 erase.addEventListener('click', function(){
     while (highscoreDiv.firstChild) {
         highscoreDiv.removeChild(highscoreDiv.firstChild);
     }
+    localStorage.clear();
+    
 });
 
 submit.addEventListener("click", function(){
@@ -15,3 +18,12 @@ submit.addEventListener("click", function(){
     h.textContent = localStorage.getItem('username') + ": " + localStorage.getItem('score');
     highscoreDiv.appendChild(h);
 })
+
+window.onload = function() {
+    scoreSpan.textContent = localStorage.getItem('score');
+    if (localStorage.getItem('username') && localStorage.getItem('score')) {
+        var h = document.createElement('p');
+        h.textContent = localStorage.getItem('username') + ": " + localStorage.getItem('score');
+        highscoreDiv.appendChild(h);
+    }
+}
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,6 +18,11 @@ var questions = [{
     title: 'String values must be enclosed within ______ when being assigned to variables',
     choices: ['commas','curly brackets','quotes','parentheses'],
     answer: 'quotes'
+  },
+  {
+    title: 'A very useful tool during development and debugging for printing content to the debugger is:',
+    choices: ['Javascript','terminal/bash','for loops','console.log'],
+    answer: 'console.log'
   }
 ];
 var startbtn = document.querySelector("#startQuizbtn");
